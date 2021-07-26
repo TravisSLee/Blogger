f550dd8dc6b1c1287be8b0e0323051d5647df979
TravisSLee <travisslee223@gmail.com>
2021-07-26 14:51:29 -0400

    update server and index

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/articles', articleRouter)
 app.get('/',(req, res) => {
     const articles = [{
         title: "Test",
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description: "testing"
     }]
     res.render('index', { articles: articles})
