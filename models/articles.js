6e9e9e1d1f926319a16355329f8f11539c6cef9f
TravisSLee <travisslee223@gmail.com>
2021-07-29 11:48:31 -0400

    implement model

---
+++ models/articles.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const artilclesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
