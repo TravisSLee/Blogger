8228ce08a08ae69656f14b67ae0fd6ee6700dbf0
TravisSLee <travisslee223@gmail.com>
2021-08-13 15:11:21 -0400

    fix routing and display errors

--- models/article.js
+++ models/article.js
@@ -6,44 +6,42 @@ const { JSDOM } = require('jsdom')
 const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    markdown: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    slug: {
-        type: String,
-        required: true,
-        unique: true
-      },
-    sanitizedHtml: {
-      type: String,
-      required: true
-    }  
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  markdown: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
+  }
 })
 
 articleSchema.pre('validate', function(next) {
-  //validations and before attributes article upon create and edit
-    if (this.title) {
-      this.slug = slugify(this.title, { lower: true, strict: true })
-    }
-  
-    if (this.markdown) {
-      this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
-    }
-  
-    next()
-  })
-  
-  module.exports = mongoose.model('Article', articleSchema)
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true })
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+  }
+
+  next()
+})
+
+module.exports = mongoose.model('Article', articleSchema)
